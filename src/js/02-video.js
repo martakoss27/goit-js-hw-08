7fd13e71e3ff769e64dcdcd5802b7d52ccfb8057
MartaKoss27 <27691497m@gmail.com>
2023-06-28 20:41:25 +0200

    update task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
 player.on(
