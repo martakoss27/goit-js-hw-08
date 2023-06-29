dafa6b2a3232496532e012c60ccaaca9955745dd
MartaKoss27 <27691497m@gmail.com>
2023-06-29 14:26:55 +0200

    done task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const { email, message } = form.elements;
+
+function getInputData(event) {
+  const inputData = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputData));
+}
+form.addEventListener('input', throttle(getInputData, 500));
+
+function clearStorage(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.clear();
+}
+
+form.addEventListener('submit', clearStorage);
